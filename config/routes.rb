7040154de42c87ea678ba5effaa9b74db9670ec2
Julien Malcouronne <malcouronnejulien@gmail.com>
2021-11-22 21:01:45 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/friends", to: "pages#friends"
   resources :friends, only: [:new, :create, :index]
+  resources :festivals
 end
