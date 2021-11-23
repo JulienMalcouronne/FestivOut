f7d3a72fdd4b5d5a48c890970f3759377ac1a097
amir fouchane <amir.fouchane@gmail.com>
2021-11-23 17:52:57 +0100

    done with the chatbox

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/friends", to: "pages#friends"
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
   resources :friends, only: [:new, :create, :index]
   resources :festivals do
     member do
