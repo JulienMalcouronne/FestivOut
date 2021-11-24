999c05410a6616fc5daa15f0a4de502a610f2cda
amir fouchane <amir.fouchane@gmail.com>
2021-11-24 11:23:16 +0100

    first fix

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,5 +2,6 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+    @festival = Festival.find(params[:festival_id])
   end
 end
