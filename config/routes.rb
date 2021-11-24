63d4e84e568181c3b643f8c040456c0c9c960e9c
Julien Malcouronne <malcouronnejulien@gmail.com>
2021-11-24 11:42:29 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/friends", to: "pages#friends"
+  mount StripeEvent::Engine, at: '/stripe-webhooks'
   resources :chatrooms, only: :show do
     resources :messages, only: :create
   end
