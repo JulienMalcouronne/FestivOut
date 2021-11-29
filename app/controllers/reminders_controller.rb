e6695e351bc7c6b121d1b93ff2612d36f7dd90fa
Julien Malcouronne <malcouronnejulien@gmail.com>
2021-11-29 17:31:27 +0100

    ok

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,7 +1,29 @@
 class RemindersController < ApplicationController
   def index
     @user = current_user
-    authorize @user
-    @reminders = policy_scope(Reminder)
+
+    # @reminders = policy_scope(Reminder)
+    @reminders = policy_scope(@user.reminders)
+    @reminder = Reminder.new
+    # authorize @user
+  end
+
+  def new
+    @reminder = Reminder.new
+    authorize @reminder
+  end
+
+  def create
+    @reminder = Reminder.new
+    @reminder.user = current_user
+    @reminder.start_time = Time.now
+    @reminder.end_time = params[:end_time]
+    authorize @reminder
+    if @reminder.save
+      redirect_to "/"
+      flash[:alert] = 'reminder successfully added'
+    else
+      render :home
+    end
   end
 end
