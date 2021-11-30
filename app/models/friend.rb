9ee0a1f67b3f73222efa72c6b447ad7082869997
Julien Malcouronne <malcouronnejulien@gmail.com>
2021-11-30 14:21:27 +0100

    ok

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,4 +1,13 @@
 class Friend < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User"
+  after_update :build_notification
+
+  private
+
+  def build_notification
+    @user = current_user
+
+    puts ""
+  end
 end
